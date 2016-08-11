53448efe2187eacb05cf7ab439d90a61e161af45
LuxAeterna <masonyuspyc@gmail.com>
2016-08-11 11:00:56 +0800

    Add files via upload

---
+++ sketch_aug08b.ino
@@ -0,0 +1,35 @@
+//Arduino sketch printing out bluetooth data
+
+char a;
+String value = "";
+boolean ledon = false;
+#define led 5
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(led, OUTPUT);
+}
+
+String getMessage(){
+String value = "";
+char a;
+
+while(Serial.available() > 0) {
+a = ((byte)Serial.read());
+value += String(a);
+}
+return value;
+}
+
+void loop()
+{
+// if (Serial.available() > 0)
+// {string = "";}
+String value = getMessage();
+if (value!=""){
+Serial.print(value);
+Serial.println();
+}
+delay(10);
+}
